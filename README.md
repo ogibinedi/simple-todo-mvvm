7d31c658293b8ae6aa45a426e08e1650143ec73c
Ogi Bin Edi <ogibinedi@gmail.com>
2022-11-13 22:02:49 +0700

    Layout and Room Entity

---
+++ README.md
@@ -0,0 +1,5 @@
+## Simple TODO MVVM Pattern With Kotlin Language
+
+### Simple TODO v.1.0
+
+Simple TODO with mvvm pattern
---
+++ app/src/main/java/com/obedigital/simpletodo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.obedigital.simpletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
